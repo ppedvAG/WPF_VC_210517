2a92e129ece1cd842de7526153d4bd6a5d6b81f3
KlaasHagge <klaash@ppedv.de>
2021-05-17 12:14:44 +0200

    Add project files.

---
+++ HelloWPF/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public partial class App : Application
+        {
+            protected override void OnStartup(StartupEventArgs e)
+            {
+                base.OnStartup(e);
+
+                //Dieser Code wird bei Start der App ausgeführt
+            }
+        }
+    }
+}
